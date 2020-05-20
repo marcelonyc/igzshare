4d853e13b6770ec0edcf6fb9335ad3b50b48b2af
Marcelo Litosky <marcelol@iguazio.com>
2020-05-20 18:12:04 +0000

    deploy-nuclio-function.ipynb

--- nuclio_trigger_mlrun/kv-to-parquet.py
+++ nuclio_trigger_mlrun/kv-to-parquet.py
@@ -29,8 +29,8 @@ mlrunctx = get_or_create_ctx('sparkjob')
 # In[19]:
 
 
-table = 'v3io://'+ str(os.path.join(mlrunctx.get_param('v3io_container','users'), mlrunctx.get_param('V3IO_USERNAME','admin'), mlrunctx.get_param('v3io_kv_table_path','examples/bank')))
-parquet_table = 'v3io://'+ str(os.path.join(mlrunctx.get_param('v3io_container','users'), mlrunctx.get_param('V3IO_USERNAME','admin'), mlrunctx.get_param('v3io_parquet_table_path','tmp/parquet')))
+#table = 'v3io://'+ str(os.path.join(mlrunctx.get_param('v3io_container','users'), mlrunctx.get_param('V3IO_USERNAME','marcelo'), mlrunctx.get_param('v3io_kv_table_path','examples/bank')))
+#parquet_table = 'v3io://'+ str(os.path.join(mlrunctx.get_param('v3io_container','users'), mlrunctx.get_param('V3IO_USERNAME','admin'), mlrunctx.get_param('v3io_parquet_table_path','tmp/parquet')))
 
 
 # In[20]:
@@ -48,15 +48,15 @@ conf = spark.sparkContext._conf
 # In[21]:
 
 
-df2 = spark.read.format("io.iguaz.v3io.spark.sql.kv").load(table)
+#df2 = spark.read.format("io.iguaz.v3io.spark.sql.kv").load(table)
 
 
 # In[22]:
 
 
-parqFile = os.path.join(parquet_table)
+#parqFile = os.path.join(parquet_table)
 
-df2.write    .mode("overwrite")    .parquet(parqFile)
+#df2.write    .mode("overwrite")    .parquet(parqFile)
 
 
 # In[ ]:
